a871cc1dc573cf06d15f37f1c4a4e90abfc899d2
湯浅 <21aw0139@jec.ac.jp>
2022-05-19 18:41:44 +0900

    three random

--- js/script.js
+++ js/script.js
@@ -11,28 +11,50 @@ const array_random = [
 ];
 
 let count = 0;
-let num = 1000;
-const under = () => {
-  const random = Math.floor(Math.random() * 3);
-  random == 0 ? (num = 10000) : random == 1 ? (num = 200) : (num = 5000);
-};
-const intervalId = setInterval(
-  () => {
-    count++;
-    const random_num = Math.floor(Math.random() * 4);
-    const value = array_random[random_num].size;
-    under();
-    box.className = `root_${value}`;
-    console.log("a");
-    if (count > 5) {
-      clearInterval(intervalId);
-      b = false;
-    }
-  },
-  num == 1 ? Math.floor(Math.random() * 4000) : Math.floor(Math.random() * 4000)
-);
-let score = 0;
 
+const intervalId_1 = setInterval(() => {
+  count++;
+  const random_num = Math.floor(Math.random() * 4);
+  const value = array_random[random_num].size;
+  box.className = `root_${value}`;
+  const random = Math.floor(Math.random() * 3);
+  if (count > 5) {
+    clearInterval(
+      random == 0 ? intervalId_1 : random == 1 ? intervalId_2 : intervalId_3
+    );
+    b = false;
+  }
+}, 3000);
+const intervalId_2 = setInterval(() => {
+  count++;
+  console.log("b");
+  const random_num = Math.floor(Math.random() * 4);
+  const value = array_random[random_num].size;
+  const random = Math.floor(Math.random() * 3);
+  box.className = `root_${value}`;
+  if (count > 5) {
+    clearInterval(
+      random == 0 ? intervalId_1 : random == 1 ? intervalId_2 : intervalId_3
+    );
+    b = false;
+  }
+}, 3000);
+const intervalId_3 = setInterval(() => {
+  count++;
+  console.log("c");
+  const random_num = Math.floor(Math.random() * 4);
+  const value = array_random[random_num].size;
+  const random = Math.floor(Math.random() * 3);
+  box.className = `root_${value}`;
+  if (count > 5) {
+    clearInterval(
+      random == 0 ? intervalId_1 : random == 1 ? intervalId_2 : intervalId_3
+    );
+    b = false;
+  }
+}, 3000);
+
+let score = 0;
 const judge = () => {
   score += 1;
   console.log(score);
@@ -42,7 +64,6 @@ const judge = () => {
 box.onclick = () => {
   b == true ? judge() : console.log("ahh");
 };
-
 // 足されている数は何回目かによって違うのではないか->あっていた
 //暴走しているのはcount
 
